fd8370530f9e3b5107fa2a6ae6dd9dd707847f43
Hassan Shahzad <hshahzad2005108277@gmail.com>
2025-04-05 17:23:56 +0100

    added time

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from datetime import datetime
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Im Hassan"}
+    return {"Hello": "World"}
 
+@app.get("/time")
+def get_time():
+    now = datetime.utcnow()
+    return {"current_time_utc": now.isoformat()}
